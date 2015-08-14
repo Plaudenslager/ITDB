c0e6ea0aa58ffa916393566d27fe6cd0a6c2a1c5
Peter A. Laudenslager <peter.laudenslager@gmail.com>
2015-08-13 21:49:10 -0700

    Added admin interfaces for the 5 models (correctly this time)

--- ITDB/ITDB_Main/admin.py
+++ ITDB/ITDB_Main/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
 from .models import Theater, Play, People, Production, Cast
+
+admin.site.register(Theater)
+admin.site.register(Play)
+admin.site.register(People)
+admin.site.register(Production)
+admin.site.register(Cast)
+
 # Register your models here.
 
