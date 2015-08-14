71183afc20b67861541d8a7b9f016464bafc8df6
Peter A. Laudenslager <peter.laudenslager@gmail.com>
2015-08-13 21:23:54 -0700

    added unicode definitions to each model class so that there is a good default to return when retrieving the class.

--- ITDB/ITDB_Main/models.py
+++ ITDB/ITDB_Main/models.py
@@ -7,14 +7,20 @@ class Theater(models.Model):
     city = models.CharField(max_length=40)
     state_or_province = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
+    def __unicode__(self):
+        return "{0}: {1}, {2}".format(self.name, self.city, self.state)
 
 class Play(models.Model):
     title = models.CharField(max_length=100)
     year_written = models.IntegerField()
+    def __unicode__(self):
+        return "{0} ({1})".format(self.title, self.year_written)
 
 class People(models.Model):
     name = models.CharField(max_length=40)
     short_bio = models.TextField()
+    def __unicode__(self):
+        return self.name
 
 class Production(models.Model):
     start_date = models.DateField()
@@ -22,6 +28,8 @@ class Production(models.Model):
     production_company = models.CharField(max_length=100)
     play = models.ForeignKey(Play)
     theater = models.ForeignKey(Theater)
+    def __unicode__(self):
+        return "{0}, {1} ({2} - {3})".format(Play(self.play), Theater(self.theater), self.start_date, self.end_date)
 
 class Cast(models.Model):
     person = models.ForeignKey(People)
