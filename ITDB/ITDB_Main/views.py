1a29239061f8d59631925439fb1ac3ab875e219e
Peter A. Laudenslager <peter.laudenslager@gmail.com>
2015-08-17 07:33:58 -0700

    Rename index.html to theaters.html

--- ITDB/ITDB_Main/views.py
+++ ITDB/ITDB_Main/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import RequestContext, loader
@@ -11,10 +12,15 @@ def index(request):
 def theaters(request):
     all_theaters_by_alpha = Theater.objects.order_by('name')
     context = RequestContext(request, {'all_theaters_by_alpha': all_theaters_by_alpha})
-    return render(request, 'ITDB_Main/index.html',context)
+    return render(request, 'ITDB_Main/theaters.html',context)
 
 def theater_detail(request, theater_id):
-    return HttpResponse("You are looking at theater %s." %theater_id)
+    try:
+        theater = Theater.objects.get(pk=theater_id)
+    except Theater.DoesNotExist:
+        raise Http404("Theater does not exist")
+
+    return render(request, 'ITDB_Main/theater_detail.html', {'theater' : theater})
 
 # page for People
 def person(request):
