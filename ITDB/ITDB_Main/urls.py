e4e6e9a1dfd0b7c6f147a801667be4dac5694e46
Peter A. Laudenslager <peter.laudenslager@gmail.com>
2015-08-16 23:28:39 -0700

    Prototype tempate for listing theaters

--- ITDB/ITDB_Main/urls.py
+++ ITDB/ITDB_Main/urls.py
@@ -2,5 +2,16 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ex: /main/
     url(r'^$', views.index, name='index'),
+    # ex: /main/theater/
+    url(r'^theater/$', views.theaters, name='theaters'),
+    # ex: /main/theater/1/
+    url(r'^theater/(?P<theater_id>[0-9]+)/$', views.theater_detail, name='theaters'),
+    # ex: /main/production/
+    # ex: /main/production/1
+    # ex: /main/people/
+    # ex: /main/people/1
+    # ex: /main/play/
+    # ex: /main/play/1
 ]
--- ITDB/ITDB_Main/views.py
+++ ITDB/ITDB_Main/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from .models import Theater
 
-# Create your views here.
+# Default first page.  Should be the search page.
 def index(request):
     return HttpResponse("Hello, world. You're at the ITDB_Main index.")
+
+# page for Theaters & theater details.  Will show the details about a theater, and a list of Productions.
+def theaters(request):
+    all_theaters_by_alpha = Theater.objects.order_by('name')
+    template = loader.get_template("ITDB_Main/index.html")
+    context = RequestContext(request, {'all_theaters_by_alpha': all_theaters_by_alpha})
+    return HttpResponse(template.render(context))
+
+def theater_detail(request, theater_id):
+    return HttpResponse("You are looking at theater %s." %theater_id)
+
+# page for People
+def person(request):
+    return HttpResponse("Page showing a single person - e.g. actor, director, writer, followed by a list of Productions")
+
+# page for Plays
+def play(request):
+    return HttpResponse("Page showing a single play, followed by a list of Productions")
+
+# page for Productions
+def production(request):
+    return HttpResponse("Page showing a single production, with details about theater and play, followed by a list of People")
